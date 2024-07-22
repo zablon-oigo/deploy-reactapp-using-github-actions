222ef05bff32fab53c513dbc94a84a4e2c92b510
zablon <zablonoigo51@gmail.com>
2024-07-22 19:22:16 +0300

    Initializng AOS animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import Aos from "aos"
+import 'aos/dist/aos.css';
+import Hero from "./components/Hero"
 
 function App() {
+  Aos.init({
+    duration:1800,
+    offset:0,
+  })
   return (
     <>
-      
+      <div className="overflow-hidden">
+      <Hero/>
+      </div>
     </>
   )
 }
