b1b8389a2dcfc7b5ebd4bc2e8eee567e85306a12
zablon <zablonoigo51@gmail.com>
2024-07-23 09:04:43 +0300

    feat: Adding Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import {FaBars} from "react-icons/fa"
+import {BsArrowRight} from "react-icons/bs"
+import NavMobile from './NavMobile'
+import Nav from './Nav'
+import Logo from '../assets/img/logo.png'
+function Header() {
+    const[navMobile,setNavMobile]=useState(false)
+  return (
+    <header className='relative z-20 px-4 mb-12 md:mb-0 md:px-0'>
+        <div className="container mx-auto">
+            <div className="relative flex items-center justify-between">
+                <div className="flex items-center gap-x-[120px]">
+                <a href=""><img src={Logo} alt="" className="" /></a>
+                <div className="hidden md:flex">
+                      <Nav/>
+                </div>
+                </div>
+                <div className={` ${navMobile ? 'max-h-52':'max-h-0'} absolute left-0 right-0 w-full overflow-hidden font-bold transition-all rounded md:hidden top-24 bg-accent-tertiary`}>
+                    <NavMobile/>
+                </div>
+                <button className='btn btn-quaternary flex items-center gap-x-[20px] group'>
+                    Request Demo <BsArrowRight className='text-2xl transition text-accent-primary group-hover:text-white'/>
+                </button>
+                <div onClick={()=>setNavMobile(!navMobile)} className="text-2xl cursor-pointer md:hidden text-primary">
+                     <FaBars/>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
