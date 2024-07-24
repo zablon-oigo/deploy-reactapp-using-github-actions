130206413d815b88c72eafe08aafc25bd5ac66c4
zablon <zablonoigo51@gmail.com>
2024-07-24 07:36:09 +0300

    feat: added aos animation effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,17 @@ export default function About() {
     const{image,title,subtitle}=aboutData
     return (
     <>
-    <section className="my-[30px] md:mt-[100px]">
+    <section className="my-[30px] md:mt-[100px]" data-aos="fade-up" data-aos-offset='350'>
         <div className="container mx-auto">
             <div className="bg-accent-secondary/[15%] rounded-[50px] px-12 pb-12 flex flex-col text-center md:flex-row md:items-center md:text-left md:gap-x-[60px] md:pb-0">
-                <div className="flex-1">
+                <div className="flex-1" data-aos='zoom-in-left'>
                 <img src={image} alt="" className="" />
                 </div>
                 <div className="flex-1 md:pr-12 ">
-                    <h2 className="mb-10 h2">
+                    <h2 className="mb-10 h2" data-aos='fade-up' data-aos-delay='300'>
                         {title}
                     </h2>
-                    <p className="max-w[474px] mx-auto md:mx-0">
+                    <p className="max-w[474px] mx-auto md:mx-0" data-aos='fade-up' data-aos-delay='400'>
                         {subtitle}
                     </p>
                 </div>
