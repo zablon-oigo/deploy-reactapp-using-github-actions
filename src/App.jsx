849a08589d7869d30729065c52b1e6b4234c6d8b
zablon <zablonoigo51@gmail.com>
2024-07-24 07:01:01 +0300

    Adding About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Aos from "aos"
 import 'aos/dist/aos.css';
 import Hero from "./components/Hero"
+import About from "./components/About";
 
 function App() {
   Aos.init({
@@ -11,6 +12,7 @@ function App() {
     <>
       <div className="overflow-hidden">
       <Hero/>
+      <About/>
       </div>
     </>
   )
