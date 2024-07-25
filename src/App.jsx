b8ae4de14112b12349ee3096341b07328eb55535
zablon <zablonoigo51@gmail.com>
2024-07-25 19:44:29 +0300

    Adding features component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Aos from "aos"
 import 'aos/dist/aos.css';
 import Hero from "./components/Hero"
 import About from "./components/About";
+import Features from "./components/Features";
 
 function App() {
   Aos.init({
@@ -13,6 +14,7 @@ function App() {
       <div className="overflow-hidden">
       <Hero/>
       <About/>
+      <Features/>
       </div>
     </>
   )
