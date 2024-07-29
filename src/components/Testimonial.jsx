b79248b2295dff1f2adfe3229a98eae9090ae0f7
zablon <zablonoigo51@gmail.com>
2024-07-29 10:42:44 +0300

    Testimonial Component

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import TestimonialSlider from '../components/TestimonialSlider'
 export default function Testimonial() {
   return (
-    <div>Testimonial</div>
+    <section className="relative">
+      <div className="container mx-auto">
+        <div className="bg-accent-primary min-h-[800px] rounded-[50px]"
+        data-aos="fade-up"
+        data-aos-offset='300'>
+          <div className="flex flex-col justify-center px-2 md:px-0 h-[800px]">
+            <h2 className="h2 text-white text-center mb-[80px]">
+              Testimonials
+            </h2>
+            <TestimonialSlider/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
