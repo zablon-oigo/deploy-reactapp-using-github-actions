495dc1ecb128d84e7dee6c4fa3763cdd20598323
zablon <zablonoigo51@gmail.com>
2024-07-30 11:16:26 +0300

    Adding Cta component

--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import {ctaData} from '../Data'
+import {BsArrowRight} from 'react-icons/bs'
 export default function CtaSection() {
     const{title,subtitle,btnText1,btnText2}=ctaData;
   return (
     <>
-    <section className="">
+    <section className="my-[75px] md:my-[150px] border-t-2">
     <div className="container mx-auto">
-        <div className="">
-            <div className="">
-                <h2 className="">
+        <div className="flex flex-col items-center justify-between md:flex-row">
+            <div className="py-12 text-center md:py-24 md:text-left">
+                <h2 className="mb-5 h2">
                     {title}
                 </h2>
-                <p className="">
+                <p className="lead">
                     {subtitle}
                 </p>
+            </div>
+            <div className="flex flex-col md:flex-row gap-y-4 gap-x-[30px] ">
+                <button className="btn btn-secondary">
+                    {btnText1}
+                </button>
+                <button className="flex items-center btn btn-quaternary gap-x-[20px] group">
+                    {btnText2}
+                    <BsArrowRight className="text-2xl transition text-accent-primary group-hover:text-white "/>
+                </button>
             </div>
         </div>
     </div>
