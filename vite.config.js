8fe3f21a1042bd1ddb4995aad62846641933b513
Zablon <143833326+zablon-oigo@users.noreply.github.com>
2024-08-29 20:05:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/warehouse/'
+  base:'/deploy-reactapp-using-github-actions/'
 })
